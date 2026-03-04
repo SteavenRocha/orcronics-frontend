8f5cc8c1bc6fd345437821eedff4a461c1a22a73
SteavenRocha <steavenrocha@gmail.com>
2026-03-03 19:48:25 -0500

    dashboard structure

---
+++ app/components/shared/AppSidebar.vue
@@ -0,0 +1,75 @@
+<template>
+    <aside id="top-bar-sidebar"
+        class="fixed top-0 left-0 z-40 w-64 h-full transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar">
+        <div class="h-full px-3 py-4 overflow-y-auto bg-neutral-primary-soft border-e border-default">
+            <a href="https://flowbite.com/" class="flex items-center ps-2.5 mb-5">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 me-3" alt="Flowbite Logo" />
+                <span class="self-center text-lg text-heading font-semibold whitespace-nowrap">Flowbite</span>
+            </a>
+            <ul class="space-y-2 mt-10 font-medium">
+                <li>
+                    <UiSideBarLink to="/" label="Dashboard">
+                        <template #icon>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="lucide lucide-house _icon_1gsq5_241" aria-hidden="true">
+                                <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
+                                <path
+                                    d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z">
+                                </path>
+                            </svg>
+                        </template>
+                    </UiSideBarLink>
+                </li>
+                <li>
+                    <UiSideBarLink to="/customers" label="Customers">
+                        <template #icon>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-building2 lucide-building-2 _icon_1gsq5_241" aria-hidden="true">
+                                <path d="M10 12h4"></path>
+                                <path d="M10 8h4"></path>
+                                <path d="M14 21v-3a2 2 0 0 0-4 0v3"></path>
+                                <path d="M6 10H4a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-2">
+                                </path>
+                                <path d="M6 21V5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v16"></path>
+                            </svg>
+                        </template>
+                        <template #badge>
+                            <span
+                                class="bg-neutral-secondary-medium border border-default-medium text-heading text-xs font-medium px-1.5 py-0.5 rounded-sm">New</span>
+                        </template>
+                    </UiSideBarLink>
+                </li>
+                <li>
+                    <UiSideBarLink to="/users" label="Users">
+                        <template #icon>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="lucide lucide-users _icon_1gsq5_241" aria-hidden="true">
+                                <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path>
+                                <path d="M16 3.128a4 4 0 0 1 0 7.744"></path>
+                                <path d="M22 21v-2a4 4 0 0 0-3-3.87"></path>
+                                <circle cx="9" cy="7" r="4"></circle>
+                            </svg>
+                        </template>
+                    </UiSideBarLink>
+                </li>
+                <!-- <li>
+                    <a href="#"
+                        class="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">
+                        <svg class="shrink-0 w-5 h-5 transition duration-75 group-hover:text-fg-brand"
+                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                            viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2" />
+                        </svg>
+                        <span class="flex-1 ms-3 whitespace-nowrap">Sign In</span>
+                    </a>
+                </li> -->
+            </ul>
+        </div>
+    </aside>
+</template>
---
+++ app/components/ui/SideBarLink.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+    to: string
+    label: string
+}>()
+</script>
+
+<template>
+    <NuxtLink :to="to"
+        class="flex items-center px-3 py-3 text-body rounded-md hover:bg-neutral-tertiary hover:text-fg-brand group"
+        active-class="bg-neutral-tertiary text-fg-brand">
+        <slot name="icon" />
+        <span class="flex-1 ms-3 whitespace-nowrap">{{ label }}</span>
+        <slot name="badge" />
+    </NuxtLink>
+</template>
